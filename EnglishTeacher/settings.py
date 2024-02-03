7e627591ba7fb56f8d2c86bb6e62857681d517f1
mandycs <mandycortessimon@gmail.com>
2024-02-03 13:57:08 +0100

    Update ALLOWED_HOSTS in settings.py

--- EnglishTeacher/settings.py
+++ EnglishTeacher/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','127.0.0.1','localhost').split(',')     
 
 
 
